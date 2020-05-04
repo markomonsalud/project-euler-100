6c1a24aebd80f66e0172c3f4fc65bd59f580c74c
markomonsalud <mfgmonsalud@gmail.com>
2020-05-04 15:52:56 +0800

    finished problem 11

--- Problem1.py
+++ Problem1.py
@@ -11,6 +11,16 @@ def multiples_of3and5(number):
         if num % 15 == 0:
             total -= num
     return total
+# TO DO: Refactor code with this
+#     x = (n-1)//3
+#     y = (n-1)//5
+#     z = (n-1)//15
+#
+#     sum_x = 3 * x * (1 + x)//2
+#     sum_y = 5 * y * (1 + y)//2
+#     sum_z = 15 * z * (1 + z)//2
+#
+#     return sum_x + sum_y - sum_z
 
 
 def main():
